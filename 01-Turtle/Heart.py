542d9df5f3fc855790350d1de9f5aa95c1e1b34e
WizelfMike <70898423+WizelfMike@users.noreply.github.com>
2020-09-24 14:49:23 +0200

    Added everything

---
+++ 01-Turtle/Heart.py
@@ -0,0 +1,26 @@
+import turtle
+
+turtle.bgcolor("black")
+turtle.pencolor("white")
+
+
+turtle.left(45)
+turtle.forward(50)
+
+for x in range(87):
+    turtle.right(2)
+    turtle.forward(2)
+
+turtle.forward(200)
+turtle.right(100)
+turtle.forward(200)
+
+for x in range(87):
+    turtle.right(2)
+    turtle.forward(2)
+
+turtle.forward(60)
+turtle.left(90)
+turtle.forward(25)
+
+turtle.done()
---
+++ README.md
@@ -0,0 +1,3 @@
+# Flex-PythonExtra
+Flex Python Extra Opdrachten van Mikey Clarke 
+Lmaaaaaoooooo
